7e539770829b895c52c242b3acb64d77e79019d8
Forrest Jones <forrestjones2010@gmail.com>
2023-02-03 12:25:17 -0600

    GDC#6

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,41 +1,42 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import './ShoppingList.css';
 
 function ShoppingList({ items }) {
-const [selectedCategory, setSelectedCategory] = useState("All");
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
-const onCategoryChange = (event) => {
-setSelectedCategory(event.target.value);
-};
+  const onCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
-const filteredItems =
-selectedCategory === "All"
-? items
-: items.filter((item) => item.category === selectedCategory);
+  const filteredItems =
+    selectedCategory === "All"
+      ? items
+      : items.filter((item) => item.category === selectedCategory);
 
-return (
-<div className="ShoppingList">
-<Filter selectedCategory={selectedCategory} onCategoryChange={onCategoryChange} />
-<ul className="Items">
-{filteredItems.map((item) => (
-<Item key={item.id} name={item.name} category={item.category} />
-))}
-</ul>
-</div>
-);
+  return (
+    <div className="ShoppingList">
+      <Filter selectedCategory={selectedCategory} onCategoryChange={onCategoryChange} />
+      <ul className="Items">
+        {filteredItems.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function Filter({ selectedCategory, onCategoryChange }) {
-return (
-<div className="Filter">
-<select name="filter" value={selectedCategory} onChange={onCategoryChange}>
-<option value="All">Filter by category</option>
-<option value="Produce">Produce</option>
-<option value="Dairy">Dairy</option>
-<option value="Dessert">Dessert</option>
-</select>
-</div>
-);
+  return (
+    <div className="Filter">
+      <select name="filter" value={selectedCategory} onChange={onCategoryChange}>
+        <option value="All">Filter by category</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+      </select>
+    </div>
+  );
 }
 
 export default ShoppingList;
